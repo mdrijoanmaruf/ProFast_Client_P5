cc25975cb9eabc9c983e8387b9927c9cca20c26c
Md Rijoan Maruf <rmsaif4430@gmail.com>
2025-06-21 16:05:01 +0600

    Added Auth Layout

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../Pages/Home/Home/Home";
 import Root from "../Layouts/Root";
+import AuthLayout from "../Layouts/AuthLayout";
+import Login from "../Pages/Authentication/Login/Login";
 
 export const router = createBrowserRouter([
     {
@@ -12,6 +14,16 @@ export const router = createBrowserRouter([
                 Component : Home
             },
             
+        ]
+    },
+    {
+        path: '/',
+        Component : AuthLayout,
+        children : [
+            {
+                path : 'login',
+                Component : Login
+            }
         ]
     }
 ])
