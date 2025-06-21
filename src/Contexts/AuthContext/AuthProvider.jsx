8a805b564ab6c3f2d43863293080b1fef3a96204
Md Rijoan Maruf <rmsaif4430@gmail.com>
2025-06-21 18:10:03 +0600

    Added auth context

---
+++ src/Contexts/AuthContext/AuthProvider.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AuthProvider = ({children}) => {
+
+  const authInfo = {
+
+  }
+  return (
+    <AuthContext value = {authInfo}>
+      {children}
+    </AuthContext>
+  )
+}
+
+export default AuthProvider
