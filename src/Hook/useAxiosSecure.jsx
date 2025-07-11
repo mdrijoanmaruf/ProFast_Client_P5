a251a36f5f6712cefbf98eba84097d46dba79c84
Md Rijoan Maruf <rmsaif4430@gmail.com>
2025-07-12 05:29:30 +0600

    JWT Firbase implemented

--- src/Hook/useAxiosSecure.jsx
+++ src/Hook/useAxiosSecure.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import axios from 'axios'
+import useAuth from './useAuth'
 
 const axiosSecure = axios.create({
     baseURL : `http://localhost:5000`
 })
 
 const useAxiosSecure = () => {
+  const {user} = useAuth()
+  axiosSecure.interceptors.request.use(config => {
+    config.headers.Authorization = `Bearer ${user.accessToken}`
+    return config;
+  }, error => {
+    return Promise.reject(error)
+  })
   return axiosSecure;
 }
 
